fb544ab0c3b25577772cdae7cfd13d930d8a7b18
Max Kobzar <nimbus2219@gmail.com>
2025-02-26 00:26:46 +0000

    text update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
       <Header />
       <main className="flex flex-grow flex-col items-center justify-between bg-main-500">
         <h1
-          className={`my-auto text-center text-6xl font-bold text-main-100 lg:text-[12rem] ${inter.className}`}
+          className={`my-auto text-center text-7xl font-bold text-main-100 lg:text-[12rem] ${inter.className}`}
         >
           Coming soon!
         </h1>
